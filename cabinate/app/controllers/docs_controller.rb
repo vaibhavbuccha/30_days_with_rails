4717afadee3d0ee15e657ce36b276e5108be6fcb
vaibhav buccha <jainvaibhav415@gmail.com>
2020-04-15 18:39:56 +0530

    edit and destroy docs

--- cabinate/app/controllers/docs_controller.rb
+++ cabinate/app/controllers/docs_controller.rb
@@ -1,5 +1,5 @@
 class DocsController < ApplicationController
-  before_action :find_doc , only: [:show,:edit,:destroy]
+  before_action :find_doc , only: [:show,:edit,:destroy,:update]
 
 
   def index
@@ -20,21 +20,28 @@ class DocsController < ApplicationController
   end
 
   def show
-    @doc = Doc.find(params[:id])
   end
 
   def edit
   end
 
   def update
+    if @doc.update(doc_params)
+      redirect_to @doc
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @doc.destroy
+    redirect_to docs_path
   end
 
   private
 
   def find_doc
+      @doc = Doc.find(params[:id])
   end
 
   def doc_params
