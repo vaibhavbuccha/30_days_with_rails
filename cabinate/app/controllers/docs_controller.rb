45dd9e0d54bf2096a839e6c28657a30f5662b929
vaibhav buccha <jainvaibhav415@gmail.com>
2020-04-15 15:34:03 +0530

    doc controller

---
+++ cabinate/app/controllers/docs_controller.rb
@@ -0,0 +1,32 @@
+class DocsController < ApplicationController
+
+  class index
+  end
+
+  class new
+  end
+
+  class show
+  end
+
+  class edit
+  end
+
+  class update
+  end
+
+  class delete
+  end
+
+  class destroy
+  end
+
+  private
+
+  class find_doc
+  end
+
+  class doc_params
+  end
+
+end
