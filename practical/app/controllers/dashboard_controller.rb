a97fb6a8a37b4d2889fd69e156f027a743abed5d
vaibhav jain <jainvaibhav415@gmail.com>
2020-04-17 19:25:49 +0530

    devise customization

--- practical/app/controllers/dashboard_controller.rb
+++ practical/app/controllers/dashboard_controller.rb
@@ -1,5 +1,6 @@
 class DashboardController < ApplicationController
   before_action :check_session , only: [:index]
+  before_action :get_user_detail, only: [:index]
   layout 'application'
   def index
   end
@@ -10,5 +11,9 @@ class DashboardController < ApplicationController
     if user_signed_in? == false
       redirect_to new_user_session_path
     end
+  end
+
+  def get_user_detail
+    @user = User.find(current_user.id)
   end
 end
