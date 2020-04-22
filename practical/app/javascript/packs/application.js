b6fd67d1699a0497db9acd1b3d7a8b94385b8eb0
vaibhav buccha <jainvaiobhav415@gmail.com>
2020-04-22 11:57:27 +0530

    jquery added

--- practical/app/javascript/packs/application.js
+++ practical/app/javascript/packs/application.js
@@ -8,11 +8,8 @@
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
-// require('jquery').start()
-// require('jquery_ujs').start()
-// require('test').start()
 require("channels")
-
+require("jquery")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
