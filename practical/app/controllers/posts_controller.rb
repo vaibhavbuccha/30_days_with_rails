e8cc0e65af0ad81022b83a0bd8e78733e2ea9870
vaibhav buccha <jainvaibhav415@gmail.com>
2020-04-24 18:36:39 +0530

    delete post using ajax and show response in sweetalert

--- practical/app/controllers/posts_controller.rb
+++ practical/app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  protect_from_forgery with: :null_session
   layout 'application'
   def index
     @posts = Post.all.order('created_at DESC').page(params[:page] || 1 ).per(4)
@@ -27,6 +28,9 @@ class PostsController < ApplicationController
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    render json: {success: "Post Successfully deleted.."}
   end
 
   private
